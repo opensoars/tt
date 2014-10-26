3c2f10953acc19a557f951af7342cac127705d58
opensoars <sam_1700@hotmail.com>
2014-10-26 15:40:56 +0100

    Travis build

--- test/mocha/main.js
+++ test/mocha/main.js
@@ -9,19 +9,29 @@ var http_server = http.createServer(rest_server.listener)
   .listen(rest_server.port);
 
 
-describe('http://localhost:80/', function (){
-  it('should return `k`', function (){
-    
-    http.get('http://localhost:80/', function (res){
+describe('tests', function (){
 
-      var d = '';
-      res.on('data', function (c){ d += c; });
+  var http_server = http.createServer(rest_server.listener)
+    .listen(rest_server.port);
 
-      res.on('end', function (){
-        assert.equal(d, 'k');
+
+  describe('GET http://localhost:80/', function (){
+    it('should return `k`', function (done){
+      
+      http.get('http://localhost:80/', function (res){
+
+        var d = '';
+        res.on('data', function (c){ d += c; });
+
+        res.on('end', function (){
+          assert.equal(d, 'k');
+          done();
+        });
+
       });
 
     });
-
   });
+
+
 });
