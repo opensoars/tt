3cc35f50119e159d6642536c0f235bb9b07ec16a
opensoars <sam_1700@hotmail.com>
2014-10-26 15:51:15 +0100

    Travis leggo

--- test/mocha/main.js
+++ test/mocha/main.js
@@ -5,31 +5,26 @@ var http = require('http');
 var rest_server = require('./../../lib/rest_server');
 
 
-var http_server = http.createServer(rest_server.listener)
-  .listen(rest_server.port);
-
-
 
 
 describe('GET http://localhost:80/', function (){
+
+  var http_server = http.createServer(rest_server.listener)
+    .listen(rest_server.port);
+
   it('should return `k`', function (done){
-    
 
-    setTimeout(function (){
-      http.get('http://localhost:80/', function (res){
+    http.get('http://localhost:80/', function (res){
 
-        var d = '';
-        res.on('data', function (c){ d += c; });
+      var d = '';
+      res.on('data', function (c){ d += c; });
 
-        res.on('end', function (){
-          assert.equal(d, 'k');
-          done();
-        });
-
+      res.on('end', function (){
+        assert.equal(d, 'k');
+        done();
       });
-    }, 500);
 
-
+    });
 
   });
 });
