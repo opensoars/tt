217ab7616593f91651fa84c5da77669c745aa6d1
opensoars <sam_1700@hotmail.com>
2014-10-26 16:07:07 +0100

    Travis leggo

--- test/mocha/main.js
+++ test/mocha/main.js
@@ -5,16 +5,17 @@ var http = require('http');
 var rest_server = require('./../../lib/rest_server');
 
 
+var http_server = http.createServer(rest_server.listener)
+  .listen(rest_server.port);
 
 
 describe('GET http://localhost:80/', function (){
 
-  var http_server = http.createServer(rest_server.listener)
-    .listen(rest_server.port);
+
 
   it('should return `k`', function (done){
 
-    http.get('http://localhost:80/', function (res){
+    http.get('http://localhost:' + rest_server.port, function (res){
 
       var d = '';
       res.on('data', function (c){ d += c; });
